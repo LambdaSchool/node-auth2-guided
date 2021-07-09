28f437709b1b41ef4c4973dfc03756fecccc8f29
Dave Kidd <david.kidd@lambdaschool.com>
2021-07-08 20:56:52 -0400

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken")
-const jwtSecret = require("../../config/secrets.js")
+const {jwtSecret} = require("../../config/secrets.js")
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
