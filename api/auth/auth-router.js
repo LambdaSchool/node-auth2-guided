8f7968a3211aaa97757a0c3affc8d7f0f10e77c6
Dave Kidd <david.kidd@lambdaschool.com>
2021-07-08 20:07:48 -0400

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -43,7 +43,11 @@ router.post('/login', checkCredentials, (req, res, next) => {
 });
 
 function makeToken(user){
-
+  const payload = {
+    subject:user.id,
+    username:user.username,
+    role:user.role
+  }
 }
 
 module.exports = router;
